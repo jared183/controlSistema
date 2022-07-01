98daf84e4f1706df4436bec448333966f2cd6a54
jared27gomez.agg@gmail.com <adal13gomez.agg@gmail.com>
2022-07-01 00:29:00 -0500

    Eliminando algunas rutas no funcionales

--- app.py
+++ app.py
@@ -4,25 +4,9 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/inicio")
+@app.route("/")
 def inicio():
     return render_template('index.html')
-
-@app.route("/")
-def login():
-    return render_template('login.html')
-
-@app.route("/password")
-def contraseña():
-    return render_template('password.html')
-
-@app.route("/register")
-def registrar():
-    return render_template('register.html')
-
-@app.route("/404")
-def error404():
-    return render_template('404.html')
 
 if __name__=='__main__':
     app.run(debug = True)
